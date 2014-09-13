b7d626b896835a5a481979dfb314b272e30374f1
Clifton Kaznocha <ckaznocha@gmail.com>
2014-09-12 23:25:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="axosoft_api",
-    version="0.1.0",
+    version="0.2.0",
     description="An Axosoft API Client",
     license="MIT",
     install_requires=["requests"],
